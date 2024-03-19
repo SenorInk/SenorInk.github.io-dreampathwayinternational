01cc280884713693a5e8fb18d1c8677a4047fb08
SeniorShh <johnwalter746@gmail.com>
2024-03-19 17:45:18 +0800

    add dream country option

--- consultation-form.js
+++ consultation-form.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var educationSelect = document.getElementById("education");
     var servicesSelect = document.getElementById("services");
     var nearestOfficeSelect = document.getElementById("nearest-office");
+    var dreamCountrySelect = document.getElementById("dream-country");
 
     
     educationSelect.addEventListener("change", function () {
@@ -17,6 +18,10 @@ document.addEventListener("DOMContentLoaded", function () {
       disableSelectOption(nearestOfficeSelect, "");
     });
 
+    nearestOfficeSelect.addEventListener("change", function () {
+      disableSelectOption(dreamCountrySelect, "");
+    });
+
     function disableSelectOption(selectElement, valueToDisable) {
       for (var i = 0; i < selectElement.options.length; i++) {
         if (selectElement.options[i].value === valueToDisable) {
@@ -27,3 +32,14 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
   });
+
+  function showOtherInput(selectElement) {
+    var otherInput = document.getElementById('other-country');
+    if (selectElement.value === 'Others') {
+      otherInput.style.display = 'block';
+      otherInput.setAttribute('required', 'required'); // set the required attribute
+    } else {
+      otherInput.style.display = 'none';
+      otherInput.removeAttribute('required'); // remove the required attribute
+    }
+  }
